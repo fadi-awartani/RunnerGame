50bcb0b58b930e4ebbc6a58097934456a85e0bbe
Fadi Awartani <fadi.alawartani@gmail.com>
2016-11-26 15:55:25 -0500

    Fixed Javadoc in DeathCommand
    Still needs Abstraction Function, Rep Invariant, and Implementations
    (possibly)

--- src/coe528/project/DeathCommand.java
+++ src/coe528/project/DeathCommand.java
@@ -1,58 +1,63 @@
 package coe528.project;
 
 /**
- * An immutable class that represents a death command was given to a character.
- * Abstraction Funtion:
+ * OVERVIEW: An immutable class that represents a death command being given to a character.
+ * 
+ * Abstraction Function:
  * 
  * Rep Invariant:
  * 
  * @author Aaron, Anjalo, Fadi
  */
 public class DeathCommand implements ICommand, IObserverSubject {
-    //OVERVIEW: DeathCommand is an mutable object. A typical DeathCommand object 
-    //would be a RunnerCharacter c with a intial time initTime.
     private final RunnerCharacter c;
     private final int initTime;
     
     /**
      * Creates an empty DeathCommand. A character must attach itself for this object to be useful.
+     * EFFECTS: Initializes instance variables.
+     * MODIFIES: None.
+     * REQUIRES: None.
      */
     public DeathCommand() {
-    //EFFECTS: creates a null RunnerCharacter with initial time(initTime) set to the present time    
         initTime = Environment.time();
         c = null;
     }
     
     /**
      * Creates a DeathCommand with a RunnerCharacter attached.
+     * EFFECTS: Initializes instance variables.
+     * MODIFIES: None.
+     * REQUIRES: A valid RunnerCharacter object.
      * @param rc The character to add.
      * @param init_time The time that the previous Command calling this method was created.
      */
     private DeathCommand(RunnerCharacter rc, int init_time) {
-    //REQUIRES: rc != null
-    //EFFECTS: Stores rc to this.c and stores the initial time to this    
         initTime = init_time;
         c = rc;
     }
     
     /**
      * Attaches a character to a clone of this object, and returns the updated object.
+     * EFFECTS: None.
+     * MODIFIES: None.
+     * REQUIRES: A valid RunnerCharacter object.
      * @param c The character to add.
      * @return The updated DeathCommand object.
      */
     @Override
     public ICommand addCharacter(RunnerCharacter rc) {
-    //REQUIRES: c != null
-    //EFFECTS: returns a new DeathCommand with rc and initial time(initTime)          
         return new DeathCommand(rc, initTime);
     }
     
     /**
      * Update character attached to this death command.
+     * EFFECTS: The attached RunnerCharacter is updated.
+     * MODIFIES: None.
+     * REQUIRES: A valid RunnerCharacter object.
      */
     @Override
     public void execute() {
-    //EFFECTS: if c!= null, then it updates the c with this     
         if(c != null) {
             c.update(this);
         }
@@ -60,11 +65,13 @@ public class DeathCommand implements ICommand, IObserverSubject {
     
     /**
      * Determines if this command should be taken care of.
+     * EFFECTS: None.
+     * MODIFIES: None.
+     * REQUIRES: None.
      * @return Always returns true in this case.
      */
     @Override
     public boolean isActive() {
-        //EFFECTS: returns true always    
         return true;
     }
 }
